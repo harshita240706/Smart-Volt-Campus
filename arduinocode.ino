8dc78d5af1766a2f9828c4865b826404c979334f
Harshita <hnsv007@gmail.com>
2026-07-31 03:25:04 +0530

    Update arduinocode.ino
    ADDED ULTRASONIC LOGIC

--- arduinocode.ino
+++ arduinocode.ino
@@ -1,11 +1,15 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <DHT.h>
-
+#include <hd44780.h>
+#include <hd44780ioClass/hd44780_I2Cexp.h>
+#include <DHT.h>
 
 #define DHTPIN 4
 #define DHTTYPE DHT11
-#define OCCUPANCY_PIN 35
+#define TRIG_PIN 17
+#define ECHO_PIN 16
+
 
 
 #define LED1 12
@@ -14,20 +18,18 @@
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 DHT dht(DHTPIN, DHTTYPE);
 
-long duration;
-float distance;
+
 
 void setup() {
 
   Serial.begin(115200);
+  pinMode(TRIG_PIN,OUTPUT);
+  pinMode(ECHO_PIN,INPUT);
 
-  pinMode(TRIG_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
+  
 
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
-  pinMode(OCCUPANCY_PIN,INPUT);
-  
 
   dht.begin();
 
@@ -48,7 +50,6 @@ void setup() {
 void loop() {
 
   // ---------- Ultrasonic ----------
-
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
 
@@ -57,10 +58,11 @@ void loop() {
 
   digitalWrite(TRIG_PIN, LOW);
 
-  duration = pulseIn(ECHO_PIN, HIGH);
-  
+  duration = pulseIn(ECHO_PIN, HIGH, 30000);
 
-  distance = duration * 0.034 / 2;
+  distance = duration * 0.0343 / 2;
+
+
 
   // ---------- DHT11 ----------
 
@@ -69,44 +71,33 @@ void loop() {
 
   // ---------- LEDs ----------
 
-  if(distance < 50)
-  {
-    digitalWrite(LED1,HIGH);
-    digitalWrite(LED2,HIGH);
-  }
-  else
-  {
-    digitalWrite(LED1,LOW);
-    digitalWrite(LED2,LOW);
-  }
+  if (distance > 0 && distance < 50) {
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, HIGH);
+} else {
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+}
 
   // ---------- LCD ----------
 
-  lcd.clear();
+ lcd.clear();
 
-  lcd.setCursor(0,0);
-  lcd.print("T:");
-  lcd.print(temp);
-  lcd.print((char)223);
-  lcd.print("C");
+lcd.setCursor(0,0);
+lcd.print("T:");
+lcd.print(temp,1);
+lcd.print((char)223);
+lcd.print("C ");
 
-  lcd.setCursor(10,0);
-  lcd.print("H:");
-  lcd.print(hum);
+lcd.setCursor(9,0);
+lcd.print("H:");
+lcd.print(hum,0);
+lcd.print("%");
 
-  lcd.setCursor(0,1);
-  lcd.print("D:");
-  lcd.print(distance);
-  lcd.print(" cm");
-  if (digitalRead(OCCUPANCY_PIN) == HIGH) {
-  digitalWrite(LED1_PIN, HIGH);
-  digitalWrite(LED2_PIN, HIGH);
-  }
-  else {
-    digitalWrite(LED1_PIN, LOW);
-    digitalWrite(LED2_PIN, LOW);
-  }
-
+lcd.setCursor(0,1);
+lcd.print("D:");
+lcd.print(distance,0);
+lcd.print(" cm");
   // ---------- Serial ----------
 
   Serial.print("Temperature : ");
